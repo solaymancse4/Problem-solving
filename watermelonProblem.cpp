999351cd2dcf14b9bc4673fea3e92aa589b07e12
Md Solayman Ahsan <60215971+solaymancse4@users.noreply.github.com>
2020-05-18 14:58:39 +0600

    watermelon problem

---
+++ watermelonProblem.cpp
@@ -0,0 +1,18 @@
+//codeforce problem watermelon by solayman
+#include <iostream>
+using namespace std;
+int main(){
+int w;
+cin>>w;
+if(w%2==0 && w>2){
+	cout<<"Yes"<<endl;
+
+}
+else{
+		cout<<"No"<<endl;
+
+}
+
+return 0;
+}
+
